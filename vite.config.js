8f3eb5b58bf19ecbccde7d7537a10036254fe7e1
Matthieu56320 <95129411+Matthieu56320@users.noreply.github.com>
2026-06-07 23:14:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'portfolio-site-portfolio.up.railway.app',
+      'www.mberthier.fr',
+      'mberthier.fr'
+    ]
+  }
 })
